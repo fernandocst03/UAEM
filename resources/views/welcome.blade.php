43f625ecb29253abf85a5c35724515107eeee6e9
fernandocst03 <fernando.dorantescst@gmail.com>
2023-05-13 22:18:37 -0600

    Main view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,11 @@
 <x-app-layout>
 
   <x-slot name='header'>
+
     <x-nav-link :href="route('welcome')" :active="true">
       {{ __('Inicio') }}
     </x-nav-link>
+
   </x-slot>
 
   <section class="flex flex-col items-center pt-8 pb-32">
@@ -13,31 +15,43 @@
         alt="Edificio principal de la UAEM" class="rounded-md">
     </section>
 
-    <section class="mt-20 flex w-full flex-col items-center gap-16">
+    <section class="flex flex-col items-center w-full gap-16 mt-20">
 
-      <article class='flex w-2/3 flex-col items-center'>
+      <article class='flex flex-col items-center w-2/3'>
         <h2 class="pb-4 text-xl font-bold text-blue-600">Misión</h2>
-        <p class='text-md text-center text-gray-700'>La UAEM es una institución educativa que forma profesionales en los
+        <p class='text-center text-gray-700 text-md'>La UAEM es una institución educativa que forma profesionales
+          en los
           niveles Medio
-          Superior y Superior, que sean competentes para la vida y líderes académicos en investigación, desarrollo y
+          Superior y Superior, que sean competentes para la vida y líderes académicos en investigación,
+          desarrollo y
           creación. Con ello contribuye a la transformación de la sociedad.
-          La docencia, la investigación y la extensión se realizan con amplias perspectivas críticas, articuladas con
-          las políticas internas y externas dentro del marco de la excelencia académica. De esta forma, la universidad
-          se constituye en un punto de encuentro de la pluralidad de pensamientos y se asume como protagonista de una
+          La docencia, la investigación y la extensión se realizan con amplias perspectivas críticas,
+          articuladas con
+          las políticas internas y externas dentro del marco de la excelencia académica. De esta forma, la
+          universidad
+          se constituye en un punto de encuentro de la pluralidad de pensamientos y se asume como protagonista
+          de una
           sociedad democrática en constante movimiento.</p>
       </article>
 
-      <article class='flex w-2/3 flex-col items-center'>
+      <article class='flex flex-col items-center w-2/3'>
         <h2 class="pb-4 text-xl font-bold text-blue-600">Visión</h2>
-        <p class='text-md text-center text-gray-700'>Para 2023, la UAEM se consolida como una institución de excelencia
+        <p class='text-center text-gray-700 text-md'>Para 2023, la UAEM se consolida como una institución de
+          excelencia
           académica,
-          sustentable, incluyente y segura, reconocida por la calidad de sus egresados, el impacto de su investigación,
-          la vinculación, la difusión de la cultura y la extensión de los servicios, posicionada en los niveles estatal,
-          regional, nacional e internacional, en un mundo interconectado a través de la innovación educativa y la
+          sustentable, incluyente y segura, reconocida por la calidad de sus egresados, el impacto de su
+          investigación,
+          la vinculación, la difusión de la cultura y la extensión de los servicios, posicionada en los
+          niveles estatal,
+          regional, nacional e internacional, en un mundo interconectado a través de la innovación educativa y
+          la
           economía del conocimiento.
-          La universidad se distingue como impulsora del cambio, por la transparencia y calidad de sus procesos
-          sustantivos y adjetivos, la consolidación de sus redes del conocimiento como el recurso de mayor valor para el
-          logro de sus objetivos y por su respuesta a la sociedad, que equilibra el pensamiento global con el actuar
+          La universidad se distingue como impulsora del cambio, por la transparencia y calidad de sus
+          procesos
+          sustantivos y adjetivos, la consolidación de sus redes del conocimiento como el recurso de mayor
+          valor para el
+          logro de sus objetivos y por su respuesta a la sociedad, que equilibra el pensamiento global con el
+          actuar
           localmente.</p>
       </article>
 
