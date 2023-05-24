3d101758921d802af3022185eb81de3360b2331f
fernandocst03 <fernando.dorantescst@gmail.com>
2023-05-24 16:14:37 -0600

    Add home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AcuerdosCU\Samara;
+use App\Models\AcuerdosCU\Sesion;
 use Illuminate\Routing\Controller as BaseController;
 
 class HomeController extends BaseController
 {
-    public function __invoke()
-    {
-        return view('welcome');
-    }
+  public function __invoke()
+  {
+    return view('welcome');
+  }
+
+  public function index_acuerdosCU()
+  {
+    $last_sesion = Sesion::orderBy('id', 'desc')->first();
+    $last_five_samaras = Samara::orderBy('id', 'desc')->take(5)->get();
+    return view('AcuerdosCU.presentacion', compact('last_sesion', 'last_five_samaras'));
+  }
 }
