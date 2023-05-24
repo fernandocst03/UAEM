f3b913ea6cd4f9d407518485219ef9825c2bb273
fernandocst03 <fernando.dorantescst@gmail.com>
2023-05-24 16:22:00 -0600

    Add navigation links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,18 +1,19 @@
-<nav x-data="{ open: false }" class="border-b border-gray-100 bg-white">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
   <!-- Primary Navigation Menu -->
-  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-    <div class="flex h-16 justify-between">
+  <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="flex justify-between h-16">
       <div class="flex items-center">
         <!-- Logo -->
-        <div class="flex shrink-0 items-center">
+        <div class="flex items-center shrink-0">
           <a href="{{ route('welcome') }}">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b4/Logo_de_la_UAEM.svg" alt="Logo UAEM" class="w-24">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b4/Logo_de_la_UAEM.svg" alt="Logo UAEM"
+              width="100" height="auto">
           </a>
         </div>
 
         <!-- Navigation Links -->
         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-          <x-nav-link :href="route('welcome')" >
+          <x-nav-link :href="route('welcome')">
             {{ __('Inicio') }}
           </x-nav-link>
         </div>
@@ -23,10 +24,10 @@
 
             <x-slot name="trigger">
               <button
-                class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none">
-                {{__('Acuerdos CU')}}
+                class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white hover:text-gray-700 focus:outline-none">
+                {{ __('Acuerdos CU') }}
                 <div class="ml-1">
-                  <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path fill-rule="evenodd"
                       d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                       clip-rule="evenodd" />
@@ -35,9 +36,29 @@
               </button>
             </x-slot>
             <x-slot name="content">
-              <x-dropdown-link>
-                {{__('Presentación')}}
+              <x-dropdown-link :href="route('acuerdos-cu')">
+                {{ __('Presentación') }}
               </x-dropdown-link>
+              <x-dropdown-link :href="route('samaras.index')">
+                {{ __('Samaras') }}
+              </x-dropdown-link>
+              <x-dropdown-link :href="route('sesiones.index')">
+                {{ __('Sesiones') }}
+              </x-dropdown-link>
+              @if (Auth::check() && Auth::user()->role->role == 'Administrador')
+                <x-dropdown-link :href="route('acuerdos.index')">
+                  {{ __('Acuerdos') }}
+                </x-dropdown-link>
+                <x-dropdown-link :href="route('rectorados.index')">
+                  {{ __('Rectorados') }}
+                </x-dropdown-link>
+                <x-dropdown-link :href="route('reporte.acuerdos')">
+                  {{ __('Reporte de acuerdos') }}
+                </x-dropdown-link>
+                <x-dropdown-link :href="route('reporte.bitacoras')">
+                  {{ __('Reporte de bitacoras') }}
+                </x-dropdown-link>
+              @endif
             </x-slot>
           </x-dropdown>
         </div>
@@ -47,10 +68,10 @@
           <x-dropdown align="right" widith="48">
             <x-slot name="trigger">
               <button
-                class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none">
-                {{__('Formato 911')}}
+                class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white hover:text-gray-700 focus:outline-none">
+                {{ __('Formato 911') }}
                 <div class="ml-1">
-                  <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path fill-rule="evenodd"
                       d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                       clip-rule="evenodd" />
@@ -59,8 +80,8 @@
               </button>
             </x-slot>
             <x-slot name="content">
-              <x-dropdown-link>
-                {{__('Presentación')}}
+              <x-dropdown-link :href="route('acuerdos-cu')">
+                {{ __('Presentación') }}
               </x-dropdown-link>
             </x-slot>
           </x-dropdown>
@@ -74,11 +95,11 @@
           <x-dropdown align="right" width="48">
             <x-slot name="trigger">
               <button
-                class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none">
+                class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white hover:text-gray-700 focus:outline-none">
                 <div>{{ Auth::user()->name }}</div>
 
                 <div class="ml-1">
-                  <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path fill-rule="evenodd"
                       d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                       clip-rule="evenodd" />
@@ -98,25 +119,25 @@
 
                 <x-dropdown-link :href="route('logout')"
                   onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+												this.closest('form').submit();">
                   {{ __('Log Out') }}
                 </x-dropdown-link>
               </form>
             </x-slot>
           </x-dropdown>
         @else
-          <div class='flex gap-3 items-center'>
-            <x-nav-link :href="route('login')" >Iniciar sesión</x-nav-link>
+          <div class='flex items-center gap-3'>
+            <x-nav-link :href="route('login')">Iniciar sesión</x-nav-link>
             <x-nav-link :href="route('register')">Registrarse</x-nav-link>
           </div>
         @endif
       </div>
 
       <!-- Hamburger -->
-      <div class="-mr-2 flex items-center sm:hidden">
+      <div class="flex items-center -mr-2 sm:hidden">
         <button @click="open = ! open"
-          class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none">
-          <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+          class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none">
+          <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
             <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex" stroke-linecap="round"
               stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
@@ -129,14 +150,14 @@
 
   <!-- Responsive Navigation Menu -->
   <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
-    <div class="space-y-1 pt-2 pb-3">
+    <div class="pt-2 pb-3 space-y-1">
       <x-responsive-nav-link :href="route('welcome')">
         {{ __('Inicio') }}
       </x-responsive-nav-link>
     </div>
 
     <!-- Responsive Settings Options -->
-    <div class="border-t border-gray-200 pt-4 pb-1">
+    <div class="pt-4 pb-1 border-t border-gray-200">
       @if (Auth::check())
         <div class="px-4">
           <div class="text-base font-medium text-gray-800">{{ Auth::user()->name }}</div>
@@ -154,7 +175,7 @@
 
             <x-responsive-nav-link :href="route('logout')"
               onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+										this.closest('form').submit();">
               {{ __('Log Out') }}
             </x-responsive-nav-link>
           </form>
