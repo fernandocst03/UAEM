fdf17fd810b7dc54b2e688f44ac060feca5fa49e
fernandocst03 <fernando.dorantescst@gmail.com>
2023-05-24 16:24:01 -0600

    Other views

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,28 +16,28 @@
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans text-gray-900 antialiased">
+<body class="font-sans antialiased text-gray-900">
   @include('layouts.navigation')
-  <div class="flex flex-col items-center bg-gray-100 py-36 sm:justify-center">
+  <div class="flex flex-col items-center bg-gray-50 py-36 sm:justify-center">
     <div>
       <a href="/">
-        <x-application-logo class="h-20 w-20 fill-current text-gray-500" />
+        <x-application-logo class="w-20 h-20 text-gray-500 fill-current" />
       </a>
     </div>
 
-    <div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+    <div class="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
       {{ $slot }}
     </div>
   </div>
 </body>
 
-<footer class="h-auto w-full bg-blue-600 py-10 px-32">
+<footer class="w-full h-auto px-32 py-10 bg-blue-600">
   <div class="w-72">
     <p class="text-2xl text-gray-900">Contacto</p>
     <div class="flex flex-col gap-3 pt-5">
-      <p class="text-gray9800 text-md font-normal">Direccion: <span class="font-normal">Av. Universidad No.
+      <p class="font-normal text-gray9800 text-md">Direccion: <span class="font-normal">Av. Universidad No.
           1001, Col Chamilpa, Cuernavaca, Morelos, México. C.P. 62209</span></p>
-      <p class="text-md font-normal text-gray-900">Telefono: <span class="font-normal">(777) 329-79-00</span></p>
+      <p class="font-normal text-gray-900 text-md">Telefono: <span class="font-normal">(777) 329-79-00</span></p>
     </div>
   </div>
 </footer>
