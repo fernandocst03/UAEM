564a163b6aa35fb1a0bb8add55469eea80b1c78b
fernandocst03 <fernando.dorantescst@gmail.com>
2023-05-29 22:27:47 -0600

    Add link to download PDF document

--- resources/views/AcuerdosCU/presentacion.blade.php
+++ resources/views/AcuerdosCU/presentacion.blade.php
@@ -105,7 +105,7 @@
                 <td>{{ $samara->rectorado->ciclo }}</td>
                 <td>{{ date('d-m-y', strtotime($samara->fecha)) }}</td>
                 <td>
-                  <div class="flex items-center justify-center gap-2">
+                  <div class="flex items-center gap-2">
                     <a href="{{ route('samaras.show', ['samara' => $samara->id]) }}" class="btn-primary">
                       Ver
                     </a>
@@ -116,13 +116,17 @@
                     @endif
                   </div>
                 <td>
-                  <a href="{{ $samara->url_archivo }}" target="_blank">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                      stroke="currentColor" class="w-6 h-6">
-                      <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                    </svg>
-                  </a>
+                  @if ($samara->url_archivo)
+                    <a href="{{ $samara->url_archivo }}" target="_blank">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                      </svg>
+                    </a>
+                  @else
+                    <p class="italic text-secondary">Sin PDF asignado</p>
+                  @endif
                 </td>
                 </td>
               </tr>
