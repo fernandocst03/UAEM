945e3a3a8170c0b716553feddcd485ecbbfab27f
fernandocst03 <fernando.dorantescst@gmail.com>
2023-06-06 09:15:00 -0600

    Increase font size

--- resources/views/components/alerts/success.blade.php
+++ resources/views/components/alerts/success.blade.php
@@ -1,6 +1,6 @@
 @props(['text'])
 
-<p class="flex items-center gap-2 px-3 py-2 text-xs text-green-900 transition delay-300 bg-green-200 border-l-4 border-green-700 rounded-sm notification"
+<p class="flex items-center gap-2 px-3 py-2 text-green-900 transition delay-300 bg-green-200 border-l-4 border-green-700 rounded-sm text-md notification"
   x-data="{ show: true }" x-show="show">
   <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-check-filled" width="24"
     height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
