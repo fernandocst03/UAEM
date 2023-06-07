f6631de9fa77e4b180a14287ec28ae4c06612a3e
fernandocst03 <fernando.dorantescst@gmail.com>
2023-06-06 22:57:38 -0600

    Add Unidad Academica controller

--- app/Http/Controllers/Formato911/UnidadAcademicaController.php
+++ app/Http/Controllers/Formato911/UnidadAcademicaController.php
@@ -4,20 +4,21 @@ namespace App\Http\Controllers\Formato911;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\formato911\UnidadAcademica;
-use App\Models\formato911\PersonalAdministrativo;
-use App\Models\formato911\Infraestructura;
-use App\Models\formato911\PersonalDocente;
-use App\Models\formato911\PersonalDocenteEdad;
-use App\Models\formato911\PersonalDocenteAntiguedad;
+use App\Models\Formato911\UnidadAcademica;
+use App\Models\Formato911\PersonalAdministrativo;
+use App\Models\Formato911\Infraestructura;
+use App\Models\Formato911\PersonalDocente;
+use App\Models\Formato911\PersonalDocenteEdad;
+use App\Models\Formato911\PersonalDocenteAntiguedad;
 
 class UnidadAcademicaController extends Controller
 {
-  public function __invoke(Request $request){
-    $id = $request->unidadAcademicaId;
+  public function __invoke(Request $request)
+  {
+    $id = $request->unidadAcademica;
     $anio = $request->anio ? $request->anio : 2023;
 
-    if($request->unidadAcademicaId){
+    if ($request->unidadAcademica) {
       $unidadAcademica = UnidadAcademica::find($id);
 
       $personalAdministrativo = PersonalAdministrativo::orderBy('anio', 'desc')
@@ -26,41 +27,34 @@ class UnidadAcademicaController extends Controller
         ->first();
 
       $personalDocente = PersonalDocente::orderBy('anio', 'desc')
-      ->unidad($id)
-      ->anio($anio)
-      ->first();
+        ->unidad($id)
+        ->anio($anio)
+        ->first();
 
       $personalDocenteAntiguedad = PersonalDocenteAntiguedad::orderBy('grupo_id')
-      ->unidad($id)
-      ->anio($anio)
-      ->get();
+        ->unidad($id)
+        ->anio($anio)
+        ->get();
 
       $personalDocentEdad = PersonalDocenteEdad::orderBy('grupo_id')
-      ->unidad($id)
-      ->anio($anio)
-      ->get();
+        ->unidad($id)
+        ->anio($anio)
+        ->get();
 
       $infraestructura = Infraestructura::orderBy('anio', 'desc')
         ->unidad($id)
         ->anio($anio)
         ->first();
 
-     /*  $datos = [];
-      foreach($personalAdministrativo as $item){
-        $datos[] = ['name' => 'Directivos', 'y'=>$item->directivo_t];
-        $datos[] = ['name' => 'Docentes', 'y'=>$item->docente_t];
-        $datos[] = ['name' => 'Docentes investigadores', 'y'=>$item->docente_investigador_t];
-        $datos[] = ['name' => 'Investigador', 'y'=>$item->investigador_t];
-      } */
-
-      return view('formato_911.unidad_academica.index', compact('unidadAcademica' , 'personalAdministrativo', 'personalDocente', 'infraestructura', 'personalDocentEdad', 'personalDocenteAntiguedad'));
+      sleep(1);
+      return view('Formato911.Unidad-Academica.index', compact('unidadAcademica', 'personalAdministrativo', 'personalDocente', 'infraestructura', 'personalDocentEdad', 'personalDocenteAntiguedad'));
     } else {
       $unidadAcademica = [];
       $personalAdministrativo = [];
       $infraestructura = [];
       $personalDocente = [];
 
-      return view('formato_911.unidad_academica.index', [
+      return view('Formato911.Unidad-Academica.index', [
         'unidadAcademica' => $unidadAcademica,
         'personalAdministrativo' => $personalAdministrativo,
         'personalDocente' => $personalDocente,
