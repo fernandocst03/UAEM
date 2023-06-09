160e1e49d74312e2035e0817a77d4872d3b77600
fernandocst03 <fernando.dorantescst@gmail.com>
2023-06-09 13:26:30 -0600

    Add confirmation delete modal

--- resources/views/AcuerdosCU/Samara/edit.blade.php
+++ resources/views/AcuerdosCU/Samara/edit.blade.php
@@ -116,13 +116,28 @@
                   <p class="text">Sesión {{ $item->sesion->sesionTipo->tipo }}</p>
                   <p class="text"> : {{ $item->sesion->fecha }}</p>
                 </div>
-                <button>
+                <button type="button" x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm')">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
                       d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                   </svg>
                 </button>
+                <x-modal name="confirm">
+                  <div class="p-5">
+                    <h4 class="title">Atencion!</h4>
+                    <p class="text">¿Seguro que quiere eliminar esta sesion?</p>
+                    <div class="flex justify-end gap-2 mt-4">
+                      <x-primary-button x-on:click="$dispatch('close')" type="button">No, cancelar</x-primary-button>
+                      <x-danger-button class="gap-2" x-data="{ loading: false }" x-on:click="loading = true">
+                        <span>Si, eliminar</span>
+                        <span x-show="loading">
+                          <x-loaders.spinner />
+                        </span>
+                      </x-danger-button>
+                    </div>
+                  </div>
+                </x-modal>
               </div>
             </form>
           </div>
